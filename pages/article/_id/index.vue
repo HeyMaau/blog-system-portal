6d4399404481552c0fa815d9c446765bd27a85e9
zhengwenbo <19802021891@139.com>
2023-02-02 14:50:25 +0800

    新建分类文章列表及文章详情路由

---
+++ pages/article/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>article</div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  validate({params}) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/category/_id/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    category
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  validate({params}) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
