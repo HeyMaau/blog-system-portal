754ea16e038e4f182e95f750722cf952da01d50e
zhengwenbo <19802021891@139.com>
2023-02-03 19:29:05 +0800

    获取分类列表增加mock地址

--- middleware/get-categories.js
+++ middleware/get-categories.js
@@ -5,4 +5,8 @@ export default async function ({$axios, store}) {
   if (response.code === CODE_SUCCESS) {
     store.commit('articleCategory/addCategoryList', response.data)
   }
+  /*const api = $axios.create()
+  api.setBaseURL('https://1c5aee02-8885-4a4f-8950-592b37b961ae.mock.pstmn.io/portal/')
+  const {data: response} = await api.get('website_info/categories')
+  store.commit('articleCategory/addCategoryList', response.data)*/
 }
