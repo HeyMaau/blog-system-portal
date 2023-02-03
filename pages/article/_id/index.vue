f759f0313b9d78464de5c2e7895b1e80f1021121
zhengwenbo <19802021891@139.com>
2023-02-03 19:47:53 +0800

    文章详情页初步添加标题和正文

--- pages/article/_id/index.vue
+++ pages/article/_id/index.vue
@@ -1,16 +1,60 @@
 <template>
-  <div>article</div>
+  <div class="article-container">
+    <h1 class="article-title">
+      {{ article.title }}
+    </h1>
+    <div v-html="article.content" class="article-content">
+
+    </div>
+  </div>
 </template>
 
 <script>
+import {CODE_SUCCESS} from "@/plugins/constants";
+
 export default {
   name: "index",
   validate({params}) {
     return /^\d+$/.test(params.id)
+  },
+  data() {
+    return {
+      article: {}
+    }
+  },
+  methods: {
+    async getArticle() {
+      const {data: response} = await this.$axios.get('article/' + this.$route.params.id)
+      if (response.code === CODE_SUCCESS) {
+        this.article = response.data
+      } else {
+        this.$message.error(response.message)
+      }
+    }
+  },
+  created() {
+    this.getArticle()
   }
 }
 </script>
 
 <style scoped>
+
+.article-container {
+  width: 700px;
+}
+
+.article-title {
+  word-break: break-word;
+  font-size: 24px;
+  line-height: 1.22;
+  font-synthesis: style;
+  font-weight: 600;
+  margin: 24px 0;
+}
+
+::v-deep .article-content img {
+  max-width: 100%;
+}
 
 </style>
