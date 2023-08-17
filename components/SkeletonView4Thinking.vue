a794db6331809b2dbf3aee542f64a868c733eacf
zhengwenbo <19802021891@139.com>
2023-08-17 10:12:15 +0800

    想法页添加骨架屏和空白视图

---
+++ components/SkeletonView4Thinking.vue
@@ -0,0 +1,72 @@
+<template>
+  <el-skeleton class="skeleton-container" animated :count="count" :loading="loading">
+    <template slot="template">
+      <div class="skeleton-author-info">
+        <el-skeleton-item class="skeleton-author-avatar" variant="image"/>
+        <div class="skeleton-author-name-sign">
+          <el-skeleton-item variant="text" style="width: 30%"/>
+          <el-skeleton-item variant="text" style="width: 30%"/>
+        </div>
+      </div>
+      <el-skeleton-item variant="h1"/>
+      <el-skeleton-item variant="text"/>
+      <el-skeleton-item variant="text"/>
+      <el-skeleton-item variant="text"/>
+      <div class="skeleton-pictures-container">
+        <el-skeleton-item class="skeleton-picture" variant="image" style="width: 163px; height: 163px;"/>
+        <el-skeleton-item class="skeleton-picture" variant="image" style="width: 163px; height: 163px;"/>
+        <el-skeleton-item class="skeleton-picture" variant="image" style="width: 163px; height: 163px;"/>
+        <el-skeleton-item class="skeleton-picture" variant="image" style="width: 163px; height: 163px;"/>
+      </div>
+    </template>
+  </el-skeleton>
+</template>
+
+<script>
+export default {
+  name: "SkeletonView4Thinking",
+  props: {count: Number, loading: Boolean}
+}
+</script>
+
+<style scoped>
+
+.skeleton-container {
+  padding: 16px 20px;
+}
+
+.skeleton-pictures-container {
+  display: flex;
+}
+
+.skeleton-picture {
+  margin-right: 5px;
+}
+
+.skeleton-author-avatar {
+  height: 38px;
+  width: 38px;
+  box-sizing: border-box;
+  border-radius: 2px;
+  margin: 0;
+}
+
+.skeleton-author-info {
+  display: flex;
+  margin-bottom: 9px;
+  margin-top: 20px;
+}
+
+.skeleton-container .skeleton-author-info:first-child {
+  margin-top: 0;
+}
+
+.skeleton-author-name-sign {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  margin-left: 14px;
+}
+
+</style>
--- pages/thinking/index.vue
+++ pages/thinking/index.vue
@@ -1,42 +1,58 @@
 <template>
-  <ThinkingList class="thinking-list card" :thinkingList="thinkingList" ref="thinkingListRef">
-    <el-pagination
-      class="thinking-list-pagination"
-      hide-on-single-page
-      @current-change="handleCurrentPageChange"
-      :page-size="currentSize"
-      :pager-count="currentSize"
-      :current-page="currentPage"
-      layout="prev, pager, next"
-      :total="total">
-    </el-pagination>
-  </ThinkingList>
+  <div>
+    <ThinkingList class="thinking-list card" :thinkingList="thinkingList" ref="thinkingListRef"
+                  v-show="!loading && thinkingList.length > 0">
+      <el-pagination
+        class="thinking-list-pagination"
+        hide-on-single-page
+        @current-change="handleCurrentPageChange"
+        :page-size="currentSize"
+        :pager-count="currentSize"
+        :current-page="currentPage"
+        layout="prev, pager, next"
+        :total="total">
+      </el-pagination>
+    </ThinkingList>
+    <SkeletonView4Thinking class="thinking-list card" :count="5" :loading="true" v-show="loading"/>
+    <EmptyView class="empty-view" v-if="!loading && thinkingList.length === 0" message="在写了在写了！！！"/>
+  </div>
 </template>
 
 <script>
 import {CODE_SUCCESS} from "../../plugins/constants";
 import ThinkingList from "../../components/ThinkingList";
+import SkeletonView4Thinking from "../../components/SkeletonView4Thinking";
 
 export default {
   name: "index",
-  components: {ThinkingList},
+  components: {ThinkingList, SkeletonView4Thinking},
   middleware: ['get-categories', 'get-authorInfo'],
   data() {
     return {
       thinkingList: [],
       currentPage: 1,
       currentSize: 5,
-      total: 0
+      total: 0,
+      loadingTimeout: false,
+      loading: true,
+      hasData: false
     }
   },
   methods: {
     async getThinkings() {
+      this.loading = true
+      this.loadingTimeout = false
+      this.hasData = false
       const {data: response} = await this.$axios.get('thinking/list', {
         params: {
           page: this.currentPage,
           size: this.currentSize
         }
       })
+      this.hasData = true
+      if (this.loadingTimeout) {
+        this.loading = false
+      }
       if (response.code === CODE_SUCCESS) {
         this.thinkingList = response.data.data
         this.thinkingList.forEach(value => {
@@ -53,9 +69,18 @@ export default {
       return imageStr.split('-');
     },
     handleCurrentPageChange(page) {
+      this.setLoadingTimeout()
       this.currentPage = page
       this.getThinkings()
       scrollTo(0, 0)
+    },
+    setLoadingTimeout() {
+      setTimeout(() => {
+        this.loadingTimeout = true
+        if (this.hasData) {
+          this.loading = false
+        }
+      }, 500)
     }
   },
   created() {
@@ -63,6 +88,9 @@ export default {
   },
   beforeMount() {
     document.title = '想法 - 卧卷'
+  },
+  mounted() {
+    this.setLoadingTimeout()
   }
 }
 </script>
@@ -72,6 +100,7 @@ export default {
 .thinking-list {
   width: 700px;
   margin-top: 10px;
+  box-sizing: border-box;
 }
 
 .card {
